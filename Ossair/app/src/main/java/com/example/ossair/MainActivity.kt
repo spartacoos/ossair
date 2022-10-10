4b347ea2ef9fe2fdce4ea9d314ea2d757e42bcf0
Carlo Pecora <spartacoosh@192.168.1.141>
2022-10-10 13:11:51 +0100

    mcu-pico data server v1

--- Ossair/app/src/main/java/com/example/ossair/MainActivity.kt
+++ Ossair/app/src/main/java/com/example/ossair/MainActivity.kt
@@ -3,26 +3,63 @@ package com.example.ossair
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ossair.data.remote.HttpRoutes
+import com.example.ossair.data.remote.PostsService
+import com.example.ossair.data.remote.dto.PostResponse
 import com.example.ossair.ui.theme.OssairTheme
 
 class MainActivity : ComponentActivity() {
+    // Networking
+    private val service = PostsService.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Networking
+            val posts = produceState<List<PostResponse>>(
+                initialValue = emptyList(),
+                producer = {
+                    value = service.getPosts()
+                }
+            )
             OssairTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF101010) //MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LazyColumn {
+                        items(posts.value) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            ) {
+                                Text(text = it.dataFirst, fontSize = 30.sp, color = Color.Green)
+                                //Spacer(modifier = Modifier.height(4.dp))
+                                Text(text = it.dataSecond, fontSize = 24.sp, color = Color.White)
+                            }
+                        }
+                    }
+
+                    //Greeting("Android")
+                    Greeting(posts.value.toString())
+                    Greeting(HttpRoutes.POSTS)
                 }
             }
         }
