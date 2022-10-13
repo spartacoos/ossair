7bbff5bc0aef35ef1927df06bf3ccff319e88d0a
Carlo Pecora <spartacoosh@192.168.1.141>
2022-10-13 21:29:30 +0100

    test: sanity check

---
+++ Ossair/app/src/test/java/com/example/ossair/SanityUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.ossair
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class SanityUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals("hi there", "hi " + "there")
+    }
+}
